8af0843c1e86f0f953c1f1ae5f635e0a3c887554
Ramchandra <Ramchandra@DESKTOP-Ramchandra>
2024-12-21 15:07:46 +0530

    First Commit

---
+++ rhishik-springboot-myfirstproject/src/test/java/com/rhrishikcodes/springboot_firstapp/RhishikSpringbootMyfirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhrishikcodes.springboot_firstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhishikSpringbootMyfirstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
